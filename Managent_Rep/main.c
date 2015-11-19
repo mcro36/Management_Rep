aea9f4deb3cf07d41aa3c8d6d6698b7e60445726
rodrigo <host@rodrigos-macbook-pro.local>
2015-11-19 21:27:07 -0200

    Initial Commit

---
+++ Managent_Rep/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Managent_Rep
+//
+//  Created by rodrigo on 11/19/15.
+//  Copyright © 2015 Rodrigo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
